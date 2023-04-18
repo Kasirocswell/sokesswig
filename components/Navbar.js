126cd14c3f19f1dd6412a9c098a29dfe0a259cd8
Kasi Reeves <Kasirocswell@Rocketmail.com>
2023-04-18 14:29:01 -0700

    Styled main page, added navbar, navbar effect, splash logo, and title

---
+++ components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+const Navbar = () => {
+  const [currentSection, setCurrentSection] = useState("");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("[data-section]");
+      const section = Array.from(sections).find((section) => {
+        const rect = section.getBoundingClientRect();
+        return rect.top <= 0 && rect.bottom >= 0;
+      });
+
+      if (section) {
+        setCurrentSection(section.getAttribute("data-section"));
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`fixed top-0 w-full ${
+        currentSection === "section1"
+          ? "bg-amber-300"
+          : currentSection === "section2"
+          ? "bg-red-600"
+          : "bg-green-100"
+      }`}
+    >
+      <div className="flex flex-row justify-center">
+        <div className="px-4">Home</div>
+        <div className="px-4">Flavors</div>
+        <div className="px-4">About</div>
+        <div className="px-4">Contact</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
