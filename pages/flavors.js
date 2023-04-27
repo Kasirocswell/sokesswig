535d3a9cac79fd2cdb2fb630470ff21942e3eb94
Lewis Meyers <meyerslewis193@gmail.com>
2023-04-27 17:51:10 -0400

    styled flavors page, added conditionally rendered switch button

--- pages/flavors.js
+++ pages/flavors.js
@@ -1,10 +1,35 @@
 // Keep prettier from going to the top
 import Navbar from "@/components/Navbar";
+import { useState } from "react";
 
 const Flavors = () => {
+  const [checked, setChecked] = useState(false);
+
+  const handleClick = () => {
+    setChecked(!checked);
+  };
   return (
     <div>
-      <Navbar></Navbar>Flavors
+      <Navbar></Navbar>
+      {!checked ? (
+        <div className="w-screen h-screen bg-amber-300 flex ">
+          <button
+            onClick={handleClick}
+            className="w-[60px] h-[30px] bg-white text-black mx-auto my-auto rounded"
+          >
+            Mango
+          </button>
+        </div>
+      ) : (
+        <div className="w-screen h-screen bg-rose-700 flex ">
+          <button
+            onClick={handleClick}
+            className="w-[100px] h-[30px] bg-white text-black mx-auto my-auto rounded"
+          >
+            Strawberry
+          </button>
+        </div>
+      )}
     </div>
   );
 };
