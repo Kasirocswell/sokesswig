77ca513bb37e8ed4482bd293f8c2f614e7644f68
Lewis Meyers <meyerslewis193@gmail.com>
2023-04-30 00:23:50 -0400

    styled and added data to carousel page

--- components/flavorCard.js
+++ components/flavorCard.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import red from "../public/red.png";
 
 const FlavorCard = ({
+  cardId,
   image,
   title,
   stats1,
@@ -13,17 +13,17 @@ const FlavorCard = ({
   description,
 }) => {
   return (
-    <div className="flex flex-col rounded-2xl mx-auto my-auto container w-[700px] h-[800px] bg-white">
-      <div className="flex flex-row image-stats-container text-black justify-between mx-[150px] mt-[75px]">
-        <Image src={red} />
-        <div className="flex-col stats-container mt-[25px]">
+    <div className="flex flex-col rounded-2xl mx-auto my-auto container w-[700px] h-[800px] bg-rose-100">
+      <div className="flex flex-row image-stats-container text-black justify-between mx-[150px] mt-[75px] mb-[10px]">
+        <Image src={image} alt="" />
+        <div className="flex-col stats-container ml-[75px] mt-[25px]">
           <h2 className="text-6xl mb-[25px]">{title}</h2>
-          <p className="text-xl mb-[5px]">{stats1}</p>
-          <p className="text-xl mb-[5px]">{stats2}</p>
-          <p className="text-xl mb-[5px]">{stats3}</p>
-          <p className="text-xl mb-[5px]">{stats4}</p>
-          <p className="text-xl mb-[5px]">{stats5}</p>
-          <p className="text-xl mb-[5px]">{stats6}</p>
+          <p className="text-2xl mb-[5px]">{stats1}</p>
+          <p className="text-2xl mb-[5px]">{stats2}</p>
+          <p className="text-2xl mb-[5px]">{stats3}</p>
+          <p className="text-2xl mb-[5px]">{stats4}</p>
+          <p className="text-2xl mb-[5px]">{stats5}</p>
+          <p className="text-2xl mb-[5px]">{stats6}</p>
         </div>
       </div>
       <div>
--- pages/flavors.js
+++ pages/flavors.js
@@ -1,16 +1,40 @@
 // Keep prettier from going to the top
 import Navbar from "@/components/Navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Carousel from "@/components/carousel";
 
 const Flavors = () => {
-  const [checked, setChecked] = useState(false);
+  const bgColors = [
+    "bg-red-400",
+    "bg-yellow-400",
+    "bg-green-400",
+    "bg-pink-400",
+    "bg-orange-400",
+    "bg-purple-400",
+    "bg-cyan-400",
+    "bg-emerald-400",
+    "bg-indigo-400",
+    "bg-rose-400",
+  ];
 
-  const handleClick = () => {
-    setChecked(!checked);
-  };
+  const [backgroundColor, setBackgroundColor] = useState();
+
+  function getRandomColor() {
+    const randomIndex = Math.floor(Math.random() * bgColors.length);
+    console.log(randomIndex);
+    setBackgroundColor(bgColors[randomIndex]);
+  }
+  useEffect(() => {
+    setInterval(() => {
+      getRandomColor();
+    }, 10000);
+  }, [5000]);
   return (
-    <div className="flex w-screen h-screen bg-sky-300">
+    <div
+      className={`flex w-screen h-screen main-bg transition duration-1000 ${
+        backgroundColor || "bg-sky-300"
+      }`}
+    >
       <Navbar></Navbar>
       <Carousel></Carousel>
     </div>
